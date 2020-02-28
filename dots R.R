756a9813b2101667a0f834d8efd8e4ea6c5d6cfd
Luke Gibson <lgibson1@terpmail.umd.edu>
2020-02-28 17:41:18 -0500

    Added Prediction Dataset

--- dots R.R
+++ dots R.R
@@ -9,7 +9,9 @@ write.csv(dat2, "dots_data_total.csv")
 dat <- read.csv("~/INST377/Food_Inspection_Build/dc2020/dots_data_time.csv")
 dat$time_of_day <- dat$hours + dat$minutes/60
 dat$time_round <- round(strptime(dat$time,format = "%Y-%m-%dT%H:%M:%OSZ"), unit="hour")
-dat$time_round <- as.POSIXct(dat$time)
+dat$time_round <- as.POSIXct(dat$time_round)
+
+dat$weekday <- weekdays(as.Date(dat$time_round))
 
 dat_weather <- read.csv("~/INST377/Food_Inspection_Build/dc2020/weather_data_csv.csv")
 dat_weather$time <- strptime(dat_weather$time,format = "%Y-%m-%dT%H:%M:%OSZ")
@@ -18,3 +20,7 @@ dat_weather$time_round <- as.POSIXct(dat_weather$time)
 dat_combined <- left_join(dat, dat_weather, by = "time_round")
 write.csv(dat_combined, "dots_data_combined.csv")
 
+
+# Add weekday
+dat <- read.csv("~/INST377/Food_Inspection_Build/dc2020/dots_data.csv")
+dat$weekday <- weekdays(as.Date(dat$time_round))
